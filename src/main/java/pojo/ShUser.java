4935b6343798e72577a76324084b4659a4b97b99
李长洵 <asd>
2020-04-01 13:46:43 +0800

    asdwa

---
+++ src/main/java/pojo/ShUser.java
@@ -0,0 +1,11 @@
+package pojo;
+
+public class ShUser {
+    private Integer UserId;
+    private String userName;
+    private String passWord;
+    private Byte isActive;
+    private Integer roleId;
+    private Long createTime;
+    private Long updateTime;
+}
