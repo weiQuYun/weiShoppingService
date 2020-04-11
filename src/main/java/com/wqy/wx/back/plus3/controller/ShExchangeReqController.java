2c12ed066e89dbb5b2662ac3a4bb47e6c1591ef0
xiaoYI999 <2940517905@qq.com>
2020-04-11 15:49:35 +0800

    添加测试

---
+++ src/main/java/com/wqy/wx/back/plus3/controller/ShExchangeReqController.java
@@ -0,0 +1,17 @@
+package com.wqy.wx.back.plus3.controller;
+
+import com.wqy.wx.back.common.Constant;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author licm
+ * @since 2020-04-11
+ */
+@Api(tags = "积分兑换申请表接口管理")
+@RestController
+@RequestMapping(Constant.MAPPING + "/exchange/req")
+public class ShExchangeReqController {
+
+}
---
+++ src/main/java/com/wqy/wx/back/plus3/service/IShExchangeReqService.java
@@ -0,0 +1,16 @@
+package com.wqy.wx.back.plus3.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wqy.wx.back.plus3.entity.ShExchangeReq;
+
+/**
+ * <p>
+ * 积分兑换申请表 服务类
+ * </p>
+ *
+ * @author licm
+ * @since 2020-04-11
+ */
+public interface IShExchangeReqService extends IService<ShExchangeReq> {
+
+}
