d78d265935908ac2f4963d206df17133313d92f9
yangShengXin <1220242737@qq.com>
2020-04-11 12:07:31 +0800

    4.11  注册，vip

--- src/main/java/com/wqy/wx/back/plus3/service/impl/ShMemberServiceImpl.java
+++ src/main/java/com/wqy/wx/back/plus3/service/impl/ShMemberServiceImpl.java
@@ -33,6 +33,7 @@ public class ShMemberServiceImpl extends ServiceImpl<ShMemberMapper, ShMember> i
     private ShMemberMapper shMemberMapper;
     @Autowired
     private ShVipMapper shVipMapper;
+
     @Override
     @Transactional
     public Boolean addMember(ShMember shMember) {
