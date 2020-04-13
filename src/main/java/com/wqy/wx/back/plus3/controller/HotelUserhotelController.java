e6802274974808e26c442ea75a36eaa0e9ec2287
李长洵 <asd>
2020-04-13 17:25:36 +0800

    优惠券实现完成

--- src/main/java/com/wqy/wx/back/plus3/controller/HotelUserhotelController.java
+++ src/main/java/com/wqy/wx/back/plus3/controller/HotelUserhotelController.java
@@ -20,34 +20,34 @@ import java.util.List;
 @RequestMapping(Constant.MAPPING+"/hotelUserhotel")
     public class HotelUserhotelController {
 
-    @Autowired
-    private IHotelUserhotelService iHotelUserhotelService;
-
-    @GetMapping("/list")
-    @ApiOperation("这里是获取所有的线下店铺地址的")
-    public List<HotelUserhotel> getAll(){
-        return iHotelUserhotelService.list();
-    }
-
-    @PostMapping("")
-    @ApiOperation("添加线下店铺")
-    public String addHotel(@RequestBody HotelUserhotel hotelUserhotel){
-        iHotelUserhotelService.save(hotelUserhotel);
-        return "添加成功";
-    }
-
-    @DeleteMapping("")
-    @ApiOperation("删除线下店铺")
-    public String deleteHotel(@RequestBody HotelUserhotel hotelUserhotel){
-        iHotelUserhotelService.remove(new QueryWrapper<>(hotelUserhotel));
-        return "删除成功";
-    }
-    @PostMapping("")
-    @ApiOperation("修改线下店铺")
-    public String updateHotel(@RequestBody HotelUserhotel hotelUserhotel){
-        iHotelUserhotelService.update(new QueryWrapper<>(hotelUserhotel));
-        return "删除成功";
-    }
+//    @Autowired
+//    private IHotelUserhotelService iHotelUserhotelService;
+//
+//    @GetMapping("/list")
+//    @ApiOperation("这里是获取所有的线下店铺地址的")
+//    public List<HotelUserhotel> getAll(){
+//        return iHotelUserhotelService.list();
+//    }
+//
+//    @PostMapping("")
+//    @ApiOperation("添加线下店铺")
+//    public String addHotel(@RequestBody HotelUserhotel hotelUserhotel){
+//        iHotelUserhotelService.save(hotelUserhotel);
+//        return "添加成功";
+//    }
+//
+//    @DeleteMapping("")
+//    @ApiOperation("删除线下店铺")
+//    public String deleteHotel(@RequestBody HotelUserhotel hotelUserhotel){
+//        iHotelUserhotelService.remove(new QueryWrapper<>(hotelUserhotel));
+//        return "删除成功";
+//    }
+//    @PostMapping("")
+//    @ApiOperation("修改线下店铺")
+//    public String updateHotel(@RequestBody HotelUserhotel hotelUserhotel){
+//        iHotelUserhotelService.update(new QueryWrapper<>(hotelUserhotel));
+//        return "删除成功";
+//    }
 
 
 
--- src/main/java/com/wqy/wx/back/plus3/service/impl/HotelUserhotelServiceImpl.java
+++ src/main/java/com/wqy/wx/back/plus3/service/impl/HotelUserhotelServiceImpl.java
@@ -2,9 +2,11 @@ package com.wqy.wx.back.plus3.service.impl;
 
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wqy.wx.back.configer.annotation.PassToken;
 import com.wqy.wx.back.plus3.entity.HotelUserhotel;
 import com.wqy.wx.back.plus3.mapper.HotelUserhotelMapper;
 import com.wqy.wx.back.plus3.service.IHotelUserhotelService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 /**
