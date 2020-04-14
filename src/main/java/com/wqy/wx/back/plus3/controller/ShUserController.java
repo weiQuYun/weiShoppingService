8d7658237cab6dc8d042da28efb82afd1475973c
yangShengXin <1220242737@qq.com>
2020-04-14 09:23:21 +0800

    4.14 user

--- src/main/java/com/wqy/wx/back/plus3/controller/ShUserController.java
+++ src/main/java/com/wqy/wx/back/plus3/controller/ShUserController.java
@@ -1,7 +1,15 @@
 package com.wqy.wx.back.plus3.controller;
 
 import com.wqy.wx.back.common.Constant;
+import com.wqy.wx.back.common.util.UUIDUtils;
+import com.wqy.wx.back.plus3.entity.HotelUserhotel;
+import com.wqy.wx.back.plus3.entity.ShUser;
+import com.wqy.wx.back.plus3.mapper.ShUserMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(Constant.MAPPING + "/user")
 public class ShUserController {
+    @Autowired
+    private ShUserMapper shUserMapper;
+    @PostMapping("")
+    @ApiOperation("添加用户")
+    public String addHotel(@RequestBody ShUser shUser){
+        shUser.setId(UUIDUtils.getCharAndNumr());
+        shUserMapper.insert(shUser);
+        return "添加成功";
+    }
+
 }
